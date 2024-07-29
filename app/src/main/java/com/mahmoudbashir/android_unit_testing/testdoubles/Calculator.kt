c5150912d9c0a6d8f7eda8e7a17b2c01ae76cea5
Mahmoud Bashir <mbashir@whitehelmet.sa>
2024-07-29 15:20:59 +0300

    test arg catching nature of mockk .

--- app/src/main/java/com/mahmoudbashir/android_unit_testing/testdoubles/Calculator.kt
+++ app/src/main/java/com/mahmoudbashir/android_unit_testing/testdoubles/Calculator.kt
@@ -1,9 +1,16 @@
 package com.mahmoudbashir.android_unit_testing.testdoubles
 
-data class Dependency1(val value:Int,val isPrem:Boolean = true)
-data class Dependency2(val value:Int,val isPrem:Boolean = true)
+data class Dependency1(val value: Int, val isPrem: Boolean = true)
+data class Dependency2(val value: Int, val isPrem: Boolean = true)
+class MathService() {
+    fun add(a: Int, b: Int) = a + b
+}
 
-class Calculator(private val dep1:Dependency1, private val dep2: Dependency2){
+class Calculator(private val dep1: Dependency1, private val dep2: Dependency2) {
     fun add() = dep1.value + dep2.value
     fun subtract() = dep1.value - dep2.value
+}
+
+class CalculatorV2(private val mathService: MathService) {
+    fun add(a: Int) = mathService.add(a, 10)
 }
--- app/src/test/java/com/mahmoudbashir/android_unit_testing/testdoubles/CalculatorTest.kt
+++ app/src/test/java/com/mahmoudbashir/android_unit_testing/testdoubles/CalculatorTest.kt
@@ -2,7 +2,9 @@ package com.mahmoudbashir.android_unit_testing.testdoubles
 
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.slot
 import io.mockk.spyk
+import io.mockk.verify
 import org.junit.Assert
 import org.junit.Test
 
@@ -39,13 +41,12 @@ class CalculatorTest {
     fun `test relaxed nature of mockk`() {
         val dependency1: Dependency1 = mockk(relaxed = true)
         val dependency2: Dependency2 = mockk(relaxed = true)
-         //every { dependency2.value } returns 15
+        //every { dependency2.value } returns 15
 
         val calculator = Calculator(dependency1, dependency2)
         val result = calculator.add()
         Assert.assertEquals(0, result)
     }
-
 
 
     /*
@@ -65,4 +66,38 @@ class CalculatorTest {
         val result = calculator.add()
         Assert.assertEquals(9, result)
     }
+
+    @Test
+    fun `test arg catching nature of mockk`() {
+        val mathService: MathService = mockk()
+        val sut = CalculatorV2(mathService)
+        val slot = slot<Int>()
+        every { mathService.add(capture(slot), any()) } answers { arg<Int>(0) + arg<Int>(1) }
+        val res = sut.add(2)
+
+        Assert.assertEquals(12, res)
+        Assert.assertEquals(2, slot.captured)
+    }
+
+    // todo searching about more other matchers with url below
+    //  (mockk.io/#custom-matchers)
+    @Test
+    fun `test arg catching nature of mockk another way`() {
+        val mathService: MathService = mockk()
+        val sut = CalculatorV2(mathService)
+        every { mathService.add(any(), any()) } answers { arg<Int>(0) + arg<Int>(1) }
+        val res = sut.add(2)
+
+        Assert.assertEquals(12, res)
+        verify {
+            mathService.add(
+                withArg {
+                    Assert.assertTrue(it == 2)
+                },
+                any()
+            )
+        }
+    }
+
+    // CoEvery{} , CoVerify{} is referring to coroutines with suspend fun
 }
